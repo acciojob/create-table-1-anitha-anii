bc44f7a6f9d8b831c81707ef5d76907de2712fb0
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-15 07:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
+  let table = document.getElementById('sampleTable');
+   let row = table.insertRow(0);
   
+   let data1 = row.insertCell(0);
+     data1.innerText = "New Cell1";
+     let data2 = row.insertCell(1);
+     data2.innerText = "New Cell2";
+
+
   
 }
